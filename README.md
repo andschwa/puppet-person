859649040fe55b2b2c9b88b8c39d720f0bcbbe6d
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-03-16 19:38:45 -0700

    Major refactor for vcsh; deprecates fresh

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 puppet-person
 =============
 
-Puppet module for setting up a "person" with fresh dotfiles and an emacs.d (highly specialized)
+Puppet module for setting up a "person" with user, group, packages,
+and optional [vcsh](https://github.com/RichiH/vcsh) dotfiles setup.
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,72 @@
-class person(
-  $person,
-  $fullname,
-  $home,
-  $password,
-  $groups,
-  $directories,
-  $dotfiles_repo,
-  $fresh_repo,
-  $fresh_path,
-  $emacs_d_repo,
-) {
+define person(
+  $user     = $title,
+  $fullname = $title,
+  $groups   = $title,
+  $home     = "/home/${title}",
+  $packages = [],
+  $password = "plaintext",
+  $shell    = "bash",
 
-  include person::user
-  include person::configurations
+  # vcsh config setup
+  $use_vcsh = true,
+  $repo,
+  $provider = git,
+  $path     = "${home}/.dotfiles"
+  ) {
+
+  ensure_packages($packages)
+
+  group { $groups:
+    ensure => present,
+  }
+
+  user { $user:
+    ensure     => present,
+    comment    => $fullname,
+    gid        => $user,
+    groups     => $groups,
+    home       => $home,
+    managehome => true,
+    password   => sha1($password),
+    shell      => $shell,
+    require    => [ Group[$groups] ],
+  }
+
+  if $use_vcsh {
+    package { [ 'vcsh', 'mr' ]:
+      ensure => installed,
+    }
+
+    vcsrepo { "${user}_dotfiles":
+      ensure   => latest,
+      source   => $repo,
+      path     => $path,
+      provider => git,
+      user     => "${user}/.dotfiles",
+    }
+
+    $configdir ="${home}/.config"
+    $mrconfig = "${home}/.mrconfig"
+
+    File {
+      ensure  => link,
+      require => Vcsrepo["${user}_dotfiles"],
+      before  => Exec["${user}_mr_update"],
+    }
+
+    file { $configdir:
+      content => "${path}/.mrconfig",
+    }
+
+    file { $mrconfig:
+      content => "${path}/.config",
+    }
+
+    exec { "${user}_mr_update":
+      command => "mr update",
+      path    => "/usr/bin",
+      user    => $user,
+      require => Package['mr'],
+    }
+  }
 }
